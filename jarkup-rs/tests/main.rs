0c2c48f2dbbe315f6408e8e87981e38cda795299
Chomolungma Shirayuki <shirayuki@46ki75.com>
2025-04-27 10:53:28 +0000

    refactor: update test cases to use jarkup_rs components instead of elmethis_json_component_types

--- jarkup-rs/tests/main.rs
+++ jarkup-rs/tests/main.rs
@@ -1,28 +1,23 @@
-use elmethis_json_component_types::ParagraphSlots;
-
 #[test]
 fn serialize() {
-    let inline =
-        elmethis_json_component_types::InlineComponent::Text(elmethis_json_component_types::Text {
-            inline: true,
-            props: elmethis_json_component_types::TextProps {
-                text: "Hello, world!".to_string(),
-                ..Default::default()
-            },
+    let inline = jarkup_rs::InlineComponent::Text(jarkup_rs::Text {
+        inline: true,
+        props: jarkup_rs::TextProps {
+            text: "Hello, world!".to_string(),
             ..Default::default()
-        });
+        },
+        ..Default::default()
+    });
 
-    let paragraph = elmethis_json_component_types::Component::BlockComponent(
-        elmethis_json_component_types::BlockComponent::Paragraph(
-            elmethis_json_component_types::Paragraph {
-                inline: false,
-                props: None,
-                slots: ParagraphSlots {
-                    default: vec![inline],
-                },
+    let paragraph = jarkup_rs::Component::BlockComponent(jarkup_rs::BlockComponent::Paragraph(
+        jarkup_rs::Paragraph {
+            inline: false,
+            props: None,
+            slots: jarkup_rs::ParagraphSlots {
+                default: vec![inline],
             },
-        ),
-    );
+        },
+    ));
 
     println!("{}", serde_json::to_string(&paragraph).unwrap());
 }
@@ -31,8 +26,7 @@ fn serialize() {
 fn deserialize() {
     let slice = include_bytes!("./seed.json").as_slice();
 
-    let results =
-        serde_json::from_slice::<Vec<elmethis_json_component_types::Component>>(slice).unwrap();
+    let results = serde_json::from_slice::<Vec<jarkup_rs::Component>>(slice).unwrap();
 
     println!("{}", serde_json::to_string(&results).unwrap());
 }
